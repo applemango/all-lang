758139b728f49500a8f474b95755df0cd779c48b
applemango <apple.mangoooo97@gmail.com>
2022-05-31 14:06:07 +0900

    2022/05/31 add q_sort => .java

---
+++ quick sort/quickSort.java
@@ -0,0 +1,33 @@
+public class quickSort {
+    public static void Sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            Sort(arr, low, pi - 1);
+            Sort(arr, pi + 1, high);
+        }
+    }
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 6, 1, 3};
+        Sort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
